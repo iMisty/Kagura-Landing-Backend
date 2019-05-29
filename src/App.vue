6477d9a45042f5cf65634ef4d2b498ad7a61e885
Miya <qianwumeng_2cyndi@qq.com>
2019-05-29 23:31:03 +0800

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <top></top>
+    <home></home>
+    <bottom></bottom>
+  </div>
+</template>
+
+<script>
+import top from './views/Top';
+import home from './views/Home';
+import bottom from './views/Bottom';
+export default {
+  components: {
+    top,home,bottom
+  },
+  props: [],
+  data () {
+    return {
+    }
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created () {},
+  mounted () {}
+}
+</script>
+<style lang="less" scoped>
+#app{
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100vh;
+  overflow: hidden;
+}
+</style>
