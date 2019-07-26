92a1c8167b95a2131844ea840575818e8dc0df43
Miya <qianwumeng_2cyndi@qq.com>
2019-07-26 22:53:01 +0800

    Create List.vue

---
+++ src/components/List.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="list">
+    1
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: [],
+  data () {
+    return {
+    }
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created () {},
+  mounted () {}
+}
+</script>
+<style lang="less" scoped>
+</style>
