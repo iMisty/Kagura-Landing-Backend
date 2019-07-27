1e94524b5ec3d24b0eb88fef56700ec12f332ae9
Miya <qianwumeng_2cyndi@qq.com>
2019-07-27 22:53:06 +0800

    Create Search.vue

---
+++ src/components/Search.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="buttons">
+    <slot></slot>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: [],
+  data () {
+    return {
+    }
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created () {},
+  mounted () {}
+}
+</script>
+<style lang="less" scoped>
+</style>
