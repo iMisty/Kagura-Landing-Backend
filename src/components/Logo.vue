c4b55394f65af11973379ef5b0d0efc101288407
Miya <qianwumeng_2cyndi@qq.com>
2019-07-27 22:53:04 +0800

    Create Logo.vue

---
+++ src/components/Logo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="logo">
+    <slot>
+      <img src="../assets/logo.svg">
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: [],
+  data () {
+    return {
+    }
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created () {},
+  mounted () {}
+}
+</script>
+<style lang="less" scoped>
+img{
+  width: 100%;
+  height: 100%;
+}
+</style>
