9f4d8e4b998e7bd0ba03cda600b232231ed20f57
Miya <qianwumeng_2cyndi@qq.com>
2019-07-28 22:53:16 +0800

    Create Bottom.vue

---
+++ src/views/Bottom.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  components: {},
+  props: [],
+  data () {
+    return {
+    }
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created () {},
+  mounted () {}
+}
+</script>
+<style lang="less" scoped>
+</style>
