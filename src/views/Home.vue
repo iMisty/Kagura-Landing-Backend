9e6e331fe4ae6c69a143a1ad37c0766fb291dedb
Miya <qianwumeng_2cyndi@qq.com>
2019-07-28 22:53:15 +0800

    Create Home.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="home">
+    <div class="medium">
+      <logo></logo>
+      <input type="text" placeholder="请输入搜索内容" name="sth">
+      <section class="buttons">
+        <search></search>
+        <search></search>
+        <search></search>
+      </section>
+    </div>
+    <div class="bottom">
+      <Text></Text>
+    </div>
+  </div>
+</template>
+
+<script>
+import logo from '../components/Logo';
+import search from '../components/Search';
+export default {
+  components: {
+    logo,search
+  },
+  props: [],
+  data() {
+    return {};
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created() {},
+  mounted() {}
+};
+</script>
+<style lang="less" scoped>
+input{
+    width: 64vw;
+    height: 3.2rem;
+    padding: 1rem;
+    font-size: 2rem;
+    border-radius: 8rem;
+    border: 1px solid #dedede;
+}
+</style>
