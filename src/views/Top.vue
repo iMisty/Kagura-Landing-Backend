f5b5b61c4cc7ec886e94e3bcac200ae394c08d08
Miya <qianwumeng_2cyndi@qq.com>
2019-07-28 22:53:12 +0800

    Create Top.vue

---
+++ src/views/Top.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="top">
+    <weather></weather>
+    <links></links>
+  </div>
+</template>
+
+<script>
+import weather from '../components/Weather';
+import links from '../components/Links';
+export default {
+  components: {
+    weather,links
+  },
+  props: [],
+  data () {
+    return {
+    }
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created () {},
+  mounted () {}
+}
+</script>
+<style lang="less" scoped>
+#top{
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+}
+</style>
