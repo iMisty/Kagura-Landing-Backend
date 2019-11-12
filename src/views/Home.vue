485b6ceae1b629c62d46b09555c9b912f38408d6
Miya <qianwumeng_2cyndi@qq.com>
2019-11-13 00:14:00 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,45 +1,18 @@
 <template>
-  <div id="home">
-    <div class="medium">
-      <logo></logo>
-      <input type="text" placeholder="请输入搜索内容" name="sth">
-      <section class="buttons">
-        <search></search>
-        <search></search>
-        <search></search>
-      </section>
-    </div>
-    <div class="bottom">
-      <Text></Text>
-    </div>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import logo from '../components/Logo';
-import search from '../components/Search';
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
 export default {
+  name: 'home',
   components: {
-    logo,search
-  },
-  props: [],
-  data() {
-    return {};
-  },
-  watch: {},
-  computed: {},
-  methods: {},
-  created() {},
-  mounted() {}
-};
-</script>
-<style lang="less" scoped>
-input{
-    width: 64vw;
-    height: 3.2rem;
-    padding: 1rem;
-    font-size: 2rem;
-    border-radius: 8rem;
-    border: 1px solid #dedede;
+    HelloWorld
+  }
 }
-</style>
+</script>
