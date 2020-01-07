79039a0b7a086942a298e75314a9bc1ae4f6c7dd
Koeri Kagura <qianwumeng_2cyndi@qq.com>
2020-01-07 18:02:10 +0800

    Update copyright date

--- src/components/copyright.vue
+++ src/components/copyright.vue
@@ -1,26 +1,28 @@
 <template>
-    <div class="copyright">
-      copyright
-    </div>
+  <div class="copyright">copyright</div>
 </template>
 
 <script>
-
 export default {
-    components: {},
-    data() {
-        return {
-
-        };
-    },
-    computed: {},
-    watch: {},
-    methods: {},
-    created() {},
-    mounted() {}
-    }
+  components: {},
+  data() {
+    return {
+			year: 0
+		};
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+  created() {},
+  mounted() {
+		const d = new Date();
+		const years = d.getFullYear();
+		this.$data.year = years;
+		// eslint-disable-next-line no-console
+		console.log(years);
+	}
+};
 </script>
 <style lang='less' scoped>
 //@import url(); 引入公共css类
-
 </style>
