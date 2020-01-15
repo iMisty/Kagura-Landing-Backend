3335fc060eb6eff6e6e6fac4656174c1f6f2855b
Koeri Kagura <qianwumeng_2cyndi@qq.com>
2020-01-15 18:19:55 +0800

    Create weather.vue
    import weather

---
+++ src/components/weather.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="weather">广州-19℃</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+		};
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+  created() {},
+  mounted() {
+	}
+};
+</script>
+<style lang='less' scoped>
+
+</style>
