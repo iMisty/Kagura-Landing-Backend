9897da9815ea76b236b96cc9e7ab926552c5d177
Koeri Kagura <qianwumeng_2cyndi@qq.com>
2020-01-16 18:29:50 +0800

    Update main.js
    Update Vuex

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue';
-//import Vuex from 'vuex';
+import Vuex from 'vuex';
 import App from './App.vue';
 //import axios from 'axios';
 import router from './router';
 import 'font-awesome/css/font-awesome.min.css';
+import store from './store/index'
 import './assets/less/normalize.less';
 
 Vue.config.productionTip = false;
@@ -12,6 +13,7 @@ Vue.config.productionTip = false;
 new Vue({
     router,
     //axios,
-    //Vuex,
+    Vuex,
+    store,
     render: h => h(App)
 }).$mount('#app')
