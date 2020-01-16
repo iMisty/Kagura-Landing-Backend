f11fa1fb3d3e22df20dba2642c64ecffb0b51dce
Koeri Kagura <qianwumeng_2cyndi@qq.com>
2020-01-16 18:29:54 +0800

    Create index.js
    Vuex

---
+++ src/store/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    // data
+    state: {
+
+    },
+    // computed
+    getters: {
+
+    },
+    // Change
+    mutations: {
+
+    },
+    // Methods
+    action: {
+
+    }
+});
+
+export default store;
