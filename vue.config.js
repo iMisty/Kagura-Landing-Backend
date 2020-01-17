1067a4a194b9469dc7175999dc6db87421e8e937
Koeri Kagura <qianwumeng_2cyndi@qq.com>
2020-01-17 18:17:42 +0800

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    chainWebpack: config => {
+        config
+            .plugin('webpack-bundle-analyzer')
+            .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
+    }
+
+}
