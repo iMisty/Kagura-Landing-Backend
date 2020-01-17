6b5dffd58828aaf3ce3e19e14679c914b7a7b16f
Koeri Kagura <qianwumeng_2cyndi@qq.com>
2020-01-17 18:17:33 +0800

    Create hitokoto.vue

---
+++ src/components/hitokoto.vue
@@ -0,0 +1,46 @@
+<template>
+ <div class="hitokoto">
+   <p>{{text}}</p>
+ </div>
+</template>
+
+<script>
+ export default {
+   data () {
+     return {
+       text: '111'
+     }
+   },
+   components: {
+
+   },
+   methods:{
+    getHitokoto() {
+			const _this = this;
+			fetch('https://v1.hitokoto.cn')
+				.then(function (res) {
+					return res.json();
+				})
+				.then(function (data) {
+					console.log(data);
+					_this.$data.text = data.hitokoto;
+				})
+				.catch(function (err) {
+					console.error(err);
+				})
+		},
+     sta(){
+       this.$data.text = this.store.hitokoto;
+     }
+   },
+   mounted(){
+     this.getHitokoto();
+     //this.sta();
+   }
+ }
+</script>
+
+<style>
+
+ 
+</style>
