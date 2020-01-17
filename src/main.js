e83d4816f794b35ca647483edb1c87465f646022
Koeri Kagura <qianwumeng_2cyndi@qq.com>
2020-01-17 18:17:26 +0800

    Remove axios

--- src/main.js
+++ src/main.js
@@ -1,18 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import App from './App.vue';
-//import axios from 'axios';
 import router from './router';
 import 'font-awesome/css/font-awesome.min.css';
 import store from './store/index'
 import './assets/less/normalize.less';
 
-Vue.config.productionTip = false;
-//Vue.prototype.axios = axios;
-
 new Vue({
     router,
-    //axios,
     Vuex,
     store,
     render: h => h(App)
