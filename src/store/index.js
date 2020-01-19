5a9ee877dfc13b9c7962f2e80eafa9b5ffe36b8b
Koeri Kagura <qianwumeng_2cyndi@qq.com>
2020-01-19 16:25:00 +0800

    Update index.js
    import test data

--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,27 @@ const store = new Vuex.Store({
     // data
     state: {
         // hitokoto
-        hitokoto: '加载中'
+        hitokoto: '加载中',
+        list: {
+            list1: {
+                name: 'Videos',
+                items: [
+                    { icon: '@/assets/imgs/youtube.png', link: 'https://www.youtube.com', text: 'Youtube', class: 'icon-youtube' },
+                    { icon: 'aaa.gif', link: 'https://www.bilibili.com', text: 'Bilibili' },
+                    { icon: 'aaa.gif', link: 'https://www.acfun.com', text: 'ACFun' },
+                    { icon: 'aaa.gif', link: 'https://www.youku.com', text: 'Youku' },
+                ]
+            },
+            list2: {
+                name: 'Designs',
+                items: [
+                    { icon: '@/assets/imgs/youtube.png', link: 'https://www.youtube.com', text: 'Youtube', class: 'fa-youtube' },
+                    { icon: 'aaa.gif', link: 'https://www.bilibili.com', text: 'Bilibili' },
+                    { icon: 'aaa.gif', link: 'https://www.acfun.com', text: 'ACFun' },
+                    { icon: 'aaa.gif', link: 'https://www.youku.com', text: 'Youku' },
+                ]
+            }
+        }
     },
     // computed
     getters: {
