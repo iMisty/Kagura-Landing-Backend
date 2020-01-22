8a9dfb71a3cee716bb1adc3ec2c871c1ba9a7f9b
Koeri Kagura <qianwumeng_2cyndi@qq.com>
2020-01-22 13:56:52 +0800

    Create mask.vue

---
+++ src/components/mask.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="mask"></div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+			
+		};
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+  created() {},
+  mounted() {}
+};
+</script>
+<style lang='less' scoped>
+.mask{
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  overflow:hidden;
+  z-index: 2;
+}
+</style>
