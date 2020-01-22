027ada9d29594a8c350a9603afcf2560315ff27a
Koeri Kagura <qianwumeng_2cyndi@qq.com>
2020-01-22 13:57:06 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -31,6 +31,72 @@ const store = new Vuex.Store({
                     { icon: 'aaa.gif', link: 'https://www.acfun.com', text: 'ACFun' },
                     { icon: 'aaa.gif', link: 'https://www.youku.com', text: 'Youku' },
                 ]
+            },
+            list3: {
+                name: 'Videos',
+                titleIconColor: "#29f",
+                class: 'fa-video-camera',
+                items: [
+                    { icon: '@/assets/imgs/youtube.png', link: 'https://www.youtube.com', text: 'Youtube', class: 'icon-youtube' },
+                    { icon: 'aaa.gif', link: 'https://www.bilibili.com', text: 'Bilibili' },
+                    { icon: 'aaa.gif', link: 'https://www.acfun.com', text: 'ACFun' },
+                    { icon: 'aaa.gif', link: 'https://www.youku.com', text: 'Youku' },
+                ]
+            },
+            list4: {
+                name: 'Designs',
+                class: 'icon-video',
+                titleIconColor: "#29f",
+                items: [
+                    { icon: '@/assets/imgs/youtube.png', link: 'https://www.youtube.com', text: 'Youtube', class: 'fa-youtube' },
+                    { icon: 'aaa.gif', link: 'https://www.bilibili.com', text: 'Bilibili' },
+                    { icon: 'aaa.gif', link: 'https://www.acfun.com', text: 'ACFun' },
+                    { icon: 'aaa.gif', link: 'https://www.youku.com', text: 'Youku' },
+                ]
+            },
+            list5: {
+                name: 'Videos',
+                titleIconColor: "#29f",
+                class: 'fa-video-camera',
+                items: [
+                    { icon: '@/assets/imgs/youtube.png', link: 'https://www.youtube.com', text: 'Youtube', class: 'icon-youtube' },
+                    { icon: 'aaa.gif', link: 'https://www.bilibili.com', text: 'Bilibili' },
+                    { icon: 'aaa.gif', link: 'https://www.acfun.com', text: 'ACFun' },
+                    { icon: 'aaa.gif', link: 'https://www.youku.com', text: 'Youku' },
+                ]
+            },
+            list6: {
+                name: 'Designs',
+                class: 'icon-video',
+                titleIconColor: "#29f",
+                items: [
+                    { icon: '@/assets/imgs/youtube.png', link: 'https://www.youtube.com', text: 'Youtube', class: 'fa-youtube' },
+                    { icon: 'aaa.gif', link: 'https://www.bilibili.com', text: 'Bilibili' },
+                    { icon: 'aaa.gif', link: 'https://www.acfun.com', text: 'ACFun' },
+                    { icon: 'aaa.gif', link: 'https://www.youku.com', text: 'Youku' },
+                ]
+            },
+            list7: {
+                name: 'Videos',
+                titleIconColor: "#29f",
+                class: 'fa-video-camera',
+                items: [
+                    { icon: '@/assets/imgs/youtube.png', link: 'https://www.youtube.com', text: 'Youtube', class: 'icon-youtube' },
+                    { icon: 'aaa.gif', link: 'https://www.bilibili.com', text: 'Bilibili' },
+                    { icon: 'aaa.gif', link: 'https://www.acfun.com', text: 'ACFun' },
+                    { icon: 'aaa.gif', link: 'https://www.youku.com', text: 'Youku' },
+                ]
+            },
+            list8: {
+                name: 'Designs',
+                class: 'icon-video',
+                titleIconColor: "#29f",
+                items: [
+                    { icon: '@/assets/imgs/youtube.png', link: 'https://www.youtube.com', text: 'Youtube', class: 'fa-youtube' },
+                    { icon: 'aaa.gif', link: 'https://www.bilibili.com', text: 'Bilibili' },
+                    { icon: 'aaa.gif', link: 'https://www.acfun.com', text: 'ACFun' },
+                    { icon: 'aaa.gif', link: 'https://www.youku.com', text: 'Youku' },
+                ]
             }
         }
     },
