88629292f1b2bf54443c708b620971dd1ccc14d1
Miya <qianwumeng_2cyndi@qq.com>
2020-01-25 21:42:31 +0800

    Create list-button.vue

---
+++ src/components/list-button.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="list-button">
+      <i class="fa" :class="this.$data.class"></i>
+    </div>
+</template>
+
+<script>
+
+export default {
+    components: {},
+    data() {
+        return {
+          class: ''
+        };
+    },
+    computed: {
+      buttons(){
+        return this.$store.state.listButton;
+      }
+    },
+    watch: {},
+    methods: {
+      getClass(){
+        const a = this.$store.state.listButton;
+        this.$data.class = a;
+      }
+    },
+    created() {},
+    mounted() {},
+    beforeCreate() {},
+    beforeMount() {},
+    beforeUpdate() {},
+    updated() {},
+    beforeDestroy() {},
+    destroyed() {},
+    activated() {},
+    }
+</script>
+<style lang='less' scoped>
+//@import url(); 引入公共css类
+
+</style>
