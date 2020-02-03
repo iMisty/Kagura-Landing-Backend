dd89a99a21b5d947bb1d9ff28953042417bd2f75
Miya <qianwumeng_2cyndi@qq.com>
2020-02-03 23:17:30 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,29 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Test from '../views/Test.vue';
 
 Vue.use(VueRouter)
 
 const routes = [{
-        path: '/',
-        name: 'home',
-        component: Home
-    },
-    {
-        path: '/admin',
-        name: 'admin',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import ('../views/Admin.vue')
-    }
+    path: '/',
+    name: 'home',
+    component: Home
+},
+{
+    path: '/admin',
+    name: 'admin',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+        import('../views/Admin.vue')
+},
+{
+    path: '/test',
+    name: 'test',
+    component: Test
+}
 ]
 
 const router = new VueRouter({
