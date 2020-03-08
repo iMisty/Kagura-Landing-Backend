0ae9e9661b8e1e9aee31126d87fd09f1e16fe147
Miya <qianwumeng_2cyndi@qq.com>
2020-03-08 19:39:12 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,21 @@
 module.exports = {
-    chainWebpack: config => {
-        config
-            .plugin('webpack-bundle-analyzer')
-            .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
-    }
+	chainWebpack: config => {
+		config
+			.plugin('webpack-bundle-analyzer')
+			.use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
+	},
+	devServer: {
+		proxy: {
+			'/weather': {
+				target: 'https://www.tianqiapi.com/',
+				// 允许跨域
+				changeOrigin: true,
+				ws: true,
+				pathRewrite: {
+					'^/weather': ''
+				}
+			}
+		}
+	}
 
 }
