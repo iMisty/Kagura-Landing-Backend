c4f93ee38e1f5609fd849678645935b6b17248ef
Miya <qianwumeng_2cyndi@qq.com>
2020-03-08 19:39:02 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,14 +2,17 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import App from './App.vue';
 import router from './router';
+import store from './store/index';
+import axios from 'axios';
 import 'font-awesome/css/font-awesome.min.css';
-import store from './store/index'
 import './assets/less/normalize.less';
 import './assets/less/global.less';
 
+Vue.prototype.$axios = axios;
 
 new Vue({
     router,
+    axios,
     Vuex,
     store,
     render: h => h(App)
