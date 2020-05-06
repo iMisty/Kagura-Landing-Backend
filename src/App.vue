0184c8ae24cf3d28785d0de6052ad448728c2e3a
Miya <qianwumeng_2cyndi@qq.com>
2020-05-06 23:13:54 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,16 @@
 <template>
   <div id="app">
-    <Home></Home>
+
   </div>
 </template>
 
-<script>
-import home from './views/Home';
-export default {
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
   components: {
-    Home: home,
-    //Admin: () => import('./views/Admin.vue'),
   },
-  data() {
-    return {
-    };
-  },
-  methods: {
-  },
-  mounted() {
-  }
-};
+})
+export default class App extends Vue {}
 </script>
 
-<style lang="less" scoped>
-#app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-
-
-</style>
