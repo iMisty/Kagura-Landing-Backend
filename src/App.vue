190d70291b35f590dc8112f9fe00b4de6349930d
Miya <qianwumeng_2cyndi@qq.com>
2020-05-25 00:06:17 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,34 @@
 <template>
-  <div id="app">
-
+  <div id="App">
+    <Home></Home>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import Home from '@/views/Home.vue';
 
 @Component({
+  // 组件注册
   components: {
-  },
-})
-export default class App extends Vue {}
-</script>
+    Home,
+  }})
+export default class App extends Vue {
+  // data
 
+  // props
+
+  // create
+  private created() {
+    const a = '';
+  }
+
+  // mounted
+  private mounted() {
+      const a = '';
+  }
+
+}
+</script>
+<style lang="less">
+</style>
