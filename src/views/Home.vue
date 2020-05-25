d4c35e176f524098a2218cf64a3f2b7107976f71
Miya <qianwumeng_2cyndi@qq.com>
2020-05-26 00:06:25 +0800

    Create Home.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    首页
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+@Component({
+  // 组件注册
+  components: {
+
+}})
+export default class Home extends Vue {
+// data
+
+// props
+
+// create
+private created() {
+  const a = '';
+}
+
+// mounted
+private mounted() {
+  const a = '';
+}
+
+}
+</script>
+<style lang="less">
+
+</style>
