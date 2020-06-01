690d2b032f9c22b8bba32ec6560941b407d5d18d
Miya <qianwumeng_2cyndi@qq.com>
2020-06-02 00:04:51 +0800

    Create Admin.vue

---
+++ src/views/Admin.vue
@@ -0,0 +1,38 @@
+<!--
+ * @Description: 后台管理
+ * @Version: 1.0
+ * @Autor: Miya
+ * @Date: 2020-06-02 00:04:25
+ * @LastEditors: Miya
+ * @LastEditTime: 2020-06-02 00:04:25
+--> 
+<template>
+  <div class="admin"></div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+@Component({
+  // 组件注册
+  components: {
+
+  }})
+export default class Admin extends Vue {
+  // data
+
+  // props
+
+  // create
+  private created() {
+
+  }
+
+  // mounted
+  private mounted() {
+
+  }
+
+}
+</script>
+<style lang="less">
+</style>
