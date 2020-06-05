a98e315efa505eb6d727db2f72d219106f6633c6
Miya <qianwumeng_2cyndi@qq.com>
2020-06-06 00:58:19 +0800

    Update: 优化样式展示

--- src/components/list.vue
+++ src/components/list.vue
@@ -4,13 +4,13 @@
  * @Autor: Miya
  * @Date: 2020-05-27 01:24:20
  * @LastEditors: Miya
- * @LastEditTime: 2020-06-04 23:53:32
+ * @LastEditTime: 2020-06-06 00:57:01
 --> 
 <template>
-  <div class="list">
+  <div class="list" ref="lists">
     <div class="list--items">
       <section class="list--item" v-for="list in listData" :key="list.index">
-        <h4 class="list--item--title">{{list.name}}</h4>
+        <h5 class="list--item--title">{{list.name}}</h5>
         <ul class="list--item--wrap">
           <li class="item" v-for="wrap in list.items" :key="wrap.index">
             <a :href="wrap.link">
@@ -30,6 +30,7 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 export default class List extends Vue {
   private listData = '';
 
+  // 获取链接列表数据
   private getListData(): void {
     const data = this.$store.state.link;
     this.listData = data;
