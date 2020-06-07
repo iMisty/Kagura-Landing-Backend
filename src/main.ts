990b21a7593015b20b19a07aa1c818bf9ce95ead
Miya <qianwumeng_2cyndi@qq.com>
2020-06-08 02:28:09 +0800

    WIP: 格式化版面

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,13 @@
  * @version: 1.0.0
  * @Author: Miya
  * @Date: 2020-05-26 21:59:19
- * @LastEditors: Please set LastEditors
- * @LastEditTime: 2020-06-06 14:53:59
+ * @LastEditors: Miya
+ * @LastEditTime: 2020-06-08 02:27:57
  */
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import {store} from './store';
+import { store } from './store';
 
 import 'font-awesome/css/font-awesome.min.css';
 
