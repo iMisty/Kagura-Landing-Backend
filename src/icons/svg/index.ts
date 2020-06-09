8f50f1a4b5384e8ed7331e1180573f7b34c6519a
Kagura Koeri <ling-miya@hotmail.com>
2020-06-09 18:14:49 +0800

    Update and rename index.ts to index.js

--- src/icons/svg/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: your name
- * @Date: 2020-06-08 17:56:43
- * @LastEditTime: 2020-06-08 17:56:44
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: /Single-Search/src/icons/svg/index.js
- */ 
