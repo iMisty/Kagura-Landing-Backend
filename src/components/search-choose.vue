8d89396cdfd2a8c71ab3f7df70747cb470a7bf15
Miya <qianwumeng_2cyndi@qq.com>
2020-06-12 00:11:01 +0800

    WIP: 拆分搜索引擎选择组件

---
+++ src/components/search-choose.vue
@@ -0,0 +1,28 @@
+<!--
+ * @Description: 选择搜索引擎组件
+ * @Version: 1.0
+ * @Autor: Miya
+ * @Date: 2020-06-11 23:32:14
+ * @LastEditors: Miya
+ * @LastEditTime: 2020-06-12 00:10:39
+--> 
+ <template>
+  <li class="choose-engine-wrap" :data-value="value">
+    <Svgicon :svgClass="icon" :iconClass="value" :iconName="value"></Svgicon>
+  </li>
+</template>
+ 
+ <script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import Svgicon from '@/components/svgicon.vue';
+@Component({
+  // 组件注册
+  components: {
+    Svgicon
+  }})
+export default class SearchChoose extends Vue {
+  private icon: string = 'icon';
+  // props
+  @Prop()public value: unknown;
+}
+ </script>
