54612137318399ebe46a74740c1043002274bfcb
iMisty <qianwumeng_2cyndi@qq.com>
2020-06-12 16:19:49 +0800

    WIP: 修改配置

--- src/components/search-choose.vue
+++ src/components/search-choose.vue
@@ -3,8 +3,8 @@
  * @Version: 1.0
  * @Autor: Miya
  * @Date: 2020-06-11 23:32:14
- * @LastEditors: Miya
- * @LastEditTime: 2020-06-12 00:11:25
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2020-06-12 15:50:19
 --> 
  <template>
   <li class="choose-engine-wrap" :data-value="value">
@@ -13,17 +13,17 @@
   </li>
 </template>
  
- <script lang="ts">
+<script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import Svgicon from '@/components/svgicon.vue';
 @Component({
   // 组件注册
   components: {
-    Svgicon
+    Svgicon,
   }})
 export default class SearchChoose extends Vue {
   private icon: string = 'icon';
   // props
-  @Prop() public value: unknown;
+  @Prop() private value: unknown;
 }
- </script>
+</script>
