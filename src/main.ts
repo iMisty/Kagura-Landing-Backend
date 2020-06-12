2d75176f0b783559352d9e0a90a948e695103799
iMisty <qianwumeng_2cyndi@qq.com>
2020-06-12 16:18:10 +0800

    WIP: 删除font-awesome

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,12 @@
  * @Author: Miya
  * @Date: 2020-05-26 21:59:19
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2020-06-08 18:22:03
+ * @LastEditTime: 2020-06-12 15:52:15
  */
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import { store } from './store';
-
-import 'font-awesome/css/font-awesome.min.css';
-
 import '@/style/style.less';
 
 Vue.config.productionTip = false;
