35fb0f4508012ebe4e26d62d28a8ff0d0fd7928d
Miya <qianwumeng_2cyndi@qq.com>
2020-06-14 06:13:28 +0800

    WIP: 增加背景功能

--- src/App.vue
+++ src/App.vue
@@ -4,24 +4,32 @@
  * @Autor: Miya
  * @Date: 2020-05-25 22:54:11
  * @LastEditors: Miya
- * @LastEditTime: 2020-06-10 00:50:20
+ * @LastEditTime: 2020-06-14 06:12:28
 --> 
 <template>
   <div id="App">
     <Home></Home>
+    <Background :isInputing="bgStatus"></Background>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
 import Home from '@/views/Home.vue';
+import Background from '@/components/background.vue';
 
 @Component({
   // 组件注册
   components: {
-    Home,
+    Home, Background,
   }})
 export default class App extends Vue {
+  private bgStatus: boolean = false;
+
+  @Watch('this.$store.state.status.isInputing')
+  private getStatus(status: boolean) {
+    console.log(status);
+  }
 
 }
 </script>
--- src/components/background.vue
+++ src/components/background.vue
@@ -4,10 +4,10 @@
  * @Autor: Miya
  * @Date: 2020-06-14 00:02:26
  * @LastEditors: Miya
- * @LastEditTime: 2020-06-14 00:04:43
+ * @LastEditTime: 2020-06-14 05:52:20
 --> 
 <template>
-  <div class="background" :style="background-image:{{backgroundImage}}"></div>
+  <div class="background" :style="bgOptions" :class="{'active': isInputing}"></div>
 </template>
  
 <script lang="ts">
@@ -18,6 +18,37 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 
   }})
 export default class Background extends Vue {
-  @Prop() public backgroundImage: unknown;
+  @Prop() private isInputing: boolean = false;
+
+  private bgOptions: object = {
+    backgroundImage: `url(${this.$store.state.status.background})`,
+  };
+
+  private getBackgroundImage(): void {
+    const bgData = this.$store.state.status.background;
+  }
+
+  private methods() {
+    this.getBackgroundImage();
+  }
 }
- </script>
+</script>
+<style lang="less" scoped>
+.background {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: -2;
+  opacity: 0.8;
+  background-repeat: no-repeat;
+  background-size: cover;
+  transition: .4s;
+}
+.active {
+  filter: blur(8px);
+  transform: scale(1.2);
+  transition: .4s;
+}
+</style>
