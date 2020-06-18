a0da55c33503b3d34e404a6c4c9615ec049cb781
iMisty <qianwumeng_2cyndi@qq.com>
2020-06-18 18:38:50 +0800

    WIP: Vue配置

---
+++ vue.config.js
@@ -0,0 +1,43 @@
+/*
+ * @Author: Miya
+ * @Date: 2020-06-18 18:17:09
+ * @LastEditTime: 2020-06-18 18:37:20
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /Single-Search/vue.config.js
+ */ 
+module.exports = {
+  // 打包输出目录
+  publicPath: './',
+  // 构建输出目录
+  outputDir: 'dist',
+  // 静态资源目录
+  assetsDir: 'static',
+  // CSS配置
+  // 查阅： https://cli.vuejs.org/zh/guide/css.html#%E5%BC%95%E7%94%A8%E9%9D%99%E6%80%81%E8%B5%84%E6%BA%90
+  css: {
+    // 提取至单独的CSS文件
+    extract: true
+  },
+  // 代理服务器配置
+  proxy: {
+    '/googleapi': {
+      // site: http://suggestqueries.google.com/complete/search?output=toolbar&hl=zh&q=前端
+      target: 'http://suggestqueries.google.com/complete/',
+      secure: false,
+      changeOrigin: true,
+    },
+    '/baiduapi': {
+      // site: http://suggestion.baidu.com/su?wd=前端
+      target: 'http://suggestion.baidu.com/',
+      secure: false,
+      changeOrigin: true,
+    },
+    '/bingapi': {
+      // site: https://api.bing.com/qsonhs.aspx?type=cb&q=前端
+      target: 'https://api.bing.com/',
+      secure: false,
+      changeOrigin: true,
+    },
+  }
+}
