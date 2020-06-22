f3617ef68e0d33b874b426d5b5484e3297b691ff
Miya <qianwumeng_2cyndi@qq.com>
2020-06-22 23:31:02 +0800

    Update dataClean.ts

--- src/services/dataClean.ts
+++ src/services/dataClean.ts
@@ -1,20 +1,21 @@
 /*
  * @Author: Miya
  * @Date: 2020-06-19 17:52:08
- * @LastEditTime: 2020-06-19 18:47:50
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2020-06-20 23:30:04
+ * @LastEditors: Miya
  * @Description: 对 axios.ts 中获取的数据进行清洗
  * @FilePath: /Single-Search/src/services/dataClean.ts
- * @return: JSON格式数据
+ * @return: 数组格式
  */
 import getSearchAPIData from './axios';
-
-// TODO: 使用正则表达式方法
+// 清洗百度数据
 const getBaiduData = async (value: string) => {
+  // TODO: 这写法真恶心,得找个时间搞个优雅点的方案
   const oldData = await getSearchAPIData(value);
-  const newData = oldData.data.toString().replace('window.baidu.sug(', '').replace(/(.*);\)/, '');
+  const start = oldData.indexOf('s:[') + 3;
+  const end = oldData.lastIndexOf(']});');
+  const newData = oldData.slice(start, end).replace(/"/g, '').split(',');
   console.log(newData);
   return newData;
 };
-
 export default getBaiduData;
