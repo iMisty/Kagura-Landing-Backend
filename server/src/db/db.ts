345b2063a9afbab8837f5f468659412ec1fa74fd
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2020-07-14 11:05:00 +0800

    Delete Server

--- server/src/db/db.ts
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: your name
- * @Date: 2020-07-03 18:11:05
- * @LastEditTime: 2020-07-03 18:11:06
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: /Single-Search/server/src/db/db.ts
- */ 
