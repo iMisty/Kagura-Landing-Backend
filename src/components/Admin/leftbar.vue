2dc294c6ae4b8bc18516b6c6e6b317702e178780
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2020-07-16 00:42:15 +0800

    WIP: 后台框架搭建

---
+++ src/components/Admin/leftbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="admin-leftbar">
+    <setting></setting>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+@Component({
+  // 组件注册
+  components: {}
+})
+export default class Admin extends Vue {}
+</script>
+
+<style lang="less"></style>
