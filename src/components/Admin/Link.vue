e630ac6fe2aae15945d8d6d379e7e78a68f45f35
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2020-07-24 00:12:12 +0800

    WIP: 后台管理系统

--- src/components/Admin/Link.vue
+++ src/components/Admin/Link.vue
@@ -2,12 +2,84 @@
  * @Author: Miya
  * @Date: 2020-07-23 15:36:36
  * @LastEditors: Miya
- * @LastEditTime: 2020-07-23 15:43:06
+ * @LastEditTime: 2020-07-23 23:50:18
  * @Description: file content
- * @FilePath: \Single-Search\src\components\Admin\Link.vue
+ * @FilePath: /Single-Search/src/components/Admin/Link.vue
 -->
 <template>
-  <div class="admin-link">link</div>
+  <div class="admin-link">
+    <div class="admin-wrap-title">Test title</div>
+    <section class="admin-link-types">
+      <header class="admin-link-types-title">
+        <input type="text" name="title" value="Test1" />
+      </header>
+      <article class="admin-link-types-wrap">
+        <div class="item">
+          <div class="item-icon"></div>
+          <input type="text" name="test" value="Google" />
+          <input type="text" name="test" value="https://www.google.com" />
+        </div>
+        <div class="item">
+          <div class="item-icon"></div>
+          <input type="text" name="test" value="Google" />
+          <input type="text" name="test" value="https://www.google.com" />
+        </div>
+        <div class="item">
+          <div class="item-icon"></div>
+          <input type="text" name="test" value="Google" />
+          <input type="text" name="test" value="https://www.google.com" />
+        </div>
+      </article>
+    </section>
+    <section class="admin-link-types">
+      <header class="admin-link-types-title">
+        <input type="text" name="title" value="Test1" />
+      </header>
+      <article class="admin-link-types-wrap">
+        <div class="item">
+          <div class="item-icon"></div>
+          <input type="text" name="test" value="Google" />
+          <input type="text" name="test" value="https://www.google.com" />
+        </div>
+
+        <div class="item">
+          <div class="item-icon"></div>
+          <input type="text" name="test" value="Google" />
+          <input type="text" name="test" value="https://www.google.com" />
+        </div>
+
+        <div class="item">
+          <div class="item-icon"></div>
+          <input type="text" name="test" value="Google" />
+          <input type="text" name="test" value="https://www.google.com" />
+        </div>
+      </article>
+    </section>
+    <section class="admin-link-types">
+      <header class="admin-link-types-title">
+        <input type="text" name="title" value="Test1" />
+      </header>
+      <article class="admin-link-types-wrap">
+        <div class="item">
+          <div class="item-icon"></div>
+          <input type="text" name="test" value="Google" />
+          <input type="text" name="test" value="https://www.google.com" />
+        </div>
+
+        <div class="item">
+          <div class="item-icon"></div>
+          <input type="text" name="test" value="Google" />
+          <input type="text" name="test" value="https://www.google.com" />
+        </div>
+
+        <div class="item">
+          <div class="item-icon"></div>
+          <input type="text" name="test" value="Google" />
+          <input type="text" name="test" value="https://www.google.com" />
+        </div>
+      </article>
+    </section>
+  </div>
 </template>
 
 <script lang="ts">
