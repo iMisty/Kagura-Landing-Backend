81a2f8dbb0277c5895fd9dd875dab27f27077ad2
Miya <qianwumeng_2cyndi@qq,com>
2020-07-24 11:35:34 +0800

    Temp: 临时取消history刷新
    防止刷新出现页面渲染失败

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,20 @@
 /*
  * @Author: Miya
  * @Date: 2020-05-27 14:28:24
- * @LastEditTime: 2020-07-23 17:39:27
+ * @LastEditTime: 2020-07-24 10:56:15
  * @LastEditors: Miya
  * @Description: In User Settings Edit
  * @FilePath: \Single-Search\src\router\index.ts
  */
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
-import Home from '../views/Home.vue';
-import Admin from '../views/Admin.vue';
+import Home from '@/views/Home.vue';
+import Admin from '@/views/Admin.vue';
 
-import AdminIndex from '../components/Admin/Index.vue';
-import AdminUser from '../components/Admin/User.vue';
-import AdminLink from '../components/Admin/Link.vue';
-import AdminExtra from '../components/Admin/Extra.vue';
+import AdminIndex from '@/components/Admin/Index.vue';
+import AdminUser from '@/components/Admin/User.vue';
+import AdminLink from '@/components/Admin/Link.vue';
+import AdminExtra from '@/components/Admin/Extra.vue';
 
 Vue.use(VueRouter);
 
@@ -51,7 +51,7 @@ const routes: RouteConfig[] = [
 ];
 
 const router = new VueRouter({
-  mode: 'history',
+  // mode: 'history',
   base: process.env.BASE_URL,
   routes
 });
