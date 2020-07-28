231ce324d572bcffc83e5e0f5808bc685e197597
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2020-07-29 01:07:51 +0800

    WIP: 登录页

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,14 +2,14 @@
  * @Author: Miya
  * @Date: 2020-07-24 17:48:54
  * @LastEditors: iMisty
- * @LastEditTime: 2020-07-28 02:00:21
+ * @LastEditTime: 2020-07-29 00:17:47
  * @Description: 登录页面
  * @FilePath: /Single-Search/src/views/Login.vue
 -->
 <template>
   <div class="login">
     <div class="login-form">
-      <header class="login-form-logo">Test Header</header>
+      <header class="login-form-logo">Test Logo</header>
       <section class="login-form-header">
         <h2 class="login-form-header-title">
           Treat her a cup of cappuccino,show me your bombs
@@ -20,21 +20,21 @@
         </p>
       </section>
       <section class="login-form-input">
-        <div class="login-form-input-username">
-          <h4>Username</h4>
-          <input type="text" v-model="info.username" />
+        <div class="login-form-input-item login-form-input-username">
+          <p>Username</p>
+          <input class="login-form-input-inputarea" type="text" v-model="info.username" />
         </div>
-        <div class="login-form-input-password">
-          <h4>Password</h4>
-          <input type="password" v-model="info.password" />
+        <div class="login-form-input-item login-form-input-password">
+          <p>Password</p>
+          <input class="login-form-input-inputarea" type="password" v-model="info.password" />
         </div>
       </section>
       <section class="login-form-options">
-        <div class="remember">
+        <div class="remember normal-cursor">
           <input class="remember-checkbox" type="checkbox" name="remember" />
           <p>Remember Me</p>
         </div>
-        <div class="forgotten">
+        <div class="forgotten normal-cursor">
           <p>Forget Password?</p>
         </div>
       </section>
