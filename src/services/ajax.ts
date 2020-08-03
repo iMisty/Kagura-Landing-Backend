64ba6c245d92832dcf7b8f1e60579c3c3e986910
Miya <qianwumeng_2cyndi@qq.com>
2020-08-03 18:35:32 +0800

    WIP: fs测试

--- src/services/ajax.ts
+++ src/services/ajax.ts
@@ -2,13 +2,13 @@
  * @Author: Miya
  * @Date: 2020-07-27 15:49:00
  * @LastEditors: Miya
- * @LastEditTime: 2020-07-27 17:26:13
+ * @LastEditTime: 2020-08-03 15:15:06
  * @Description: axios方法封装
  * @FilePath: \Single-Search\src\services\ajax.ts
  */
 import axios from 'axios';
 
-export const GET = (url: string, data: object, token?: string | null) => {
+export const GET = (url: string, data?: object, token?: string | null) => {
   return axios({
     method: 'get',
     headers: {
