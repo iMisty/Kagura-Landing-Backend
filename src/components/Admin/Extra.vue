be3d243ec38335aa65cd0c8ef24aa1c73178acfd
Miya <qianwumeng_2cyndi@qq.com>
2020-08-04 18:23:35 +0800

    Delete: 删除测试内容

--- src/components/Admin/Extra.vue
+++ src/components/Admin/Extra.vue
@@ -2,7 +2,7 @@
  * @Author: Miya
  * @Date: 2020-07-23 16:19:15
  * @LastEditors: Miya
- * @LastEditTime: 2020-08-03 18:19:22
+ * @LastEditTime: 2020-08-04 10:55:34
  * @Description: file content
  * @FilePath: \Single-Search\src\components\Admin\Extra.vue
 -->
@@ -14,7 +14,6 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import { GET } from '../../services/ajax';
 @Component({})
 export default class Extra extends Vue {}
 </script>
