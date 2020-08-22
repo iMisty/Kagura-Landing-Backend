10e4fb503405b6196e24a9de2c5074ee8df458a6
Miya <qianwumeng_2cyndi@qq.com>
2020-08-23 00:54:09 +0800

    Create background.vue

---
+++ src/components/Home/bottom/background.vue
@@ -0,0 +1,31 @@
+<!--
+ * @Author: Miya
+ * @Date: 2020-06-24 21:38:13
+ * @LastEditors: Miya
+ * @LastEditTime: 2020-08-24 00:25:35
+ * @Description: 背景图片设置
+ * @FilePath: /Single-Search/src/components/Home/bottom/background.vue
+-->
+
+<template>
+  <div
+    class="background"
+    :class="{ 'bg-active': isInputing }"
+    :style="bgOptions"
+  ></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import '../../../style/home/bottom/background.less';
+@Component({})
+export default class Background extends Vue {
+  @Prop() private isInputing: any = false;
+
+  // 存储从 Vuex 中获取的背景图片
+  private bgOptions: object = {
+    // backgroundImage: `url(${this.$store.state.settings.background})`
+  };
+}
+</script>
+<style lang="less"></style>
