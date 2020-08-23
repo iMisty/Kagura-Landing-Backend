b7dd0854e2a89aed72471e956c34f78c6b2081c9
Miya <qianwumeng_2cyndi@qq.com>
2020-08-24 00:56:10 +0800

    Create hitokoto.vue

---
+++ src/components/Home/bottom/hitokoto.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Description: 一言模块
+ * @Version: 1.0
+ * @Autor: Miya
+ * @Date: 2020-05-27 01:24:20
+ * @LastEditors: Miya
+ * @LastEditTime: 2020-08-23 23:49:02
+-->
+<template>
+  <div class="hitokoto">{{ hito }}</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+@Component({})
+export default class Hitokoto extends Vue {
+  @Prop({ default: '加载中...', type: String }) public hito!: string;
+}
+</script>
+<style lang="less"></style>
