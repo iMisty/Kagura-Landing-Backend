7da78a132cc91b3a1dbc5b430171607030ed408a
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2020-08-28 16:15:54 +0800

    WIP: CSS重组

--- src/views/test.tsx
+++ src/views/test.tsx
@@ -1,9 +1,16 @@
 import { Component, Vue } from 'vue-property-decorator';
+import input from '@/components/input.tsx';
 @Component({
-  components: {}
+  components: {
+    'm-input': input
+  }
 })
 export default class Test extends Vue {
   private render() {
-    return <div class="test" style="color: #fff;"></div>;
+    return (
+      <div style="width: 100vw;height: 100vh;display: flex;justify-content: center;align-items: center;">
+      <m-input type="text"></m-input>
+      </div>
+    );
   }
 }
