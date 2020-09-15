b42415ff174e1bd6e0c293582baa61f99191303c
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2020-09-15 18:47:24 +0800

    WIP: 链接修改页面

--- src/layout/admin/link.tsx
+++ src/layout/admin/link.tsx
@@ -1,4 +1,5 @@
 import { Component, Vue } from 'vue-property-decorator';
+import mButton from '@/components/Mermaid/button';
 
 interface Link {
   icon: string;
@@ -7,15 +8,50 @@ interface Link {
   titleIconColor: string;
 }
 @Component({
-  components: {}
+  components: {
+    'm-button': mButton
+  }
 })
 export default class AdminLink extends Vue {
-  private linkData: Object[] = [];
+  private saveIcon: object = require('@/assets/save.svg');
+  private deleteIcon: object = require('@/assets/delete.svg');
 
+  private linkData: Link[] = [];
+
+  /**
+   * @description: 获取数据（初始数据取自Vuex，后续数据取自localStorage）
+   * @param {type}
+   * @return {type}
+   */
   private getLinkData() {
-    const data = this.$store.state.link;
+    const data = localStorage.getItem('user_link') || this.$store.state.link;
     console.log(data);
     this.linkData = data;
+  }
+
+  /**
+   * @description: 新建一组新链接
+   * @param {type}
+   * @return {type}
+   */
+
+  private createNewItem(e) {
+    const index = e.target.parentElement.getAttribute('data-index');
+    this.linkData[index].items.push({
+      icon: '',
+      link: '',
+      text: ''
+    });
+    console.log(this.linkData[index].items);
+  }
+
+  private handleDeleteItems(e: {
+    target: { getAttribute: (arg0: string) => number };
+  }) {
+    const ids = e.target.getAttribute('data-id');
+    const index = e.target.getAttribute('data-index');
+    this.linkData[ids].items.splice(index, 1);
+    console.log(`对应数据：${JSON.stringify(this.linkData[ids].items[index])}`);
   }
 
   private mounted() {
@@ -24,31 +60,86 @@ export default class AdminLink extends Vue {
 
   private render() {
     return (
-      <div class="admin-link">
-        <section class="admin-link-list">
-          {this.linkData.map((item: any) => {
+      <div class="admin__link">
+        <section class="admin__link--list">
+          {this.linkData.map((item: any, index: number) => {
             return (
-              <table border="1">
-                <tr>
-                  <th>1</th>
-                  <th>2</th>
-                  <th>3</th>
+              <table class="admin__link--table">
+                <tr class="admin__link--table--wrap">
+                  <th class="admin__link--table--title">图标class</th>
+                  <th class="admin__link--table--title">标题</th>
+                  <th class="admin__link--table--title">标题颜色</th>
+                  <th class="admin__link--table--setting">
+                    <div class="admin__link--table--setting-button">
+                      <img src={this.deleteIcon} alt="" />
+                    </div>
+                  </th>
                 </tr>
-                <tr>
-                  <td>{item.name}</td>
-                  <td>{item.titleIconColor}</td>
-                  <td>{item.icon}</td>
+                <tr class="admin__link--table--wrap">
+                  <td class="admin__link--table--icon">
+                    <input value={item.icon} />
+                  </td>
+                  <td class="admin__link--table--link">
+                    <input value={item.name} />
+                  </td>
+                  <td class="admin__link--table--text">
+                    <input value={item.titleIconColor} />
+                  </td>
+                  <td class="admin__link--table--setting">
+                    <div class="admin__link--table--setting-button">
+                      <img src={this.deleteIcon} alt="" />
+                    </div>
+                  </td>
+                </tr>
+                <tr class="admin__link--table--wrap">
+                  <th class="admin__link--table--title">图标</th>
+                  <th class="admin__link--table--title">链接</th>
+                  <th class="admin__link--table--title">显示文字</th>
+                  <th class="admin__link--table--title">操作</th>
                 </tr>
 
-                {item.items.map(item => {
-                  return (
-                    <tr>
-                      <td>{item.icon}</td>
-                      <td>{item.link}</td>
-                      <td>{item.text}</td>
-                    </tr>
-                  );
-                })}
+                {item.items.map(
+                  (
+                    childItem: { icon: string; link: string; text: string },
+                    childIndex: number
+                  ) => {
+                    return (
+                      <tr
+                        class="admin__link--table--wrap"
+                        data-id={index}
+                        data-index={childIndex}
+                      >
+                        <td class="admin__link--table--icon">
+                          <input value={childItem.icon} />
+                        </td>
+                        <td class="admin__link--table--link">
+                          <input value={childItem.link} />
+                        </td>
+                        <td class="admin__link--table--text">
+                          <input value={childItem.text} />
+                        </td>
+                        <td class="admin__link--table--setting">
+                          <div
+                            class="admin__link--table--setting-button"
+                            onClick={this.handleDeleteItems}
+                          >
+                            <img
+                              src={this.deleteIcon}
+                              alt=""
+                              data-id={index}
+                              data-index={childIndex}
+                            />
+                          </div>
+                        </td>
+                      </tr>
+                    );
+                  }
+                )}
+                <th class="admin__link--options" data-index={index}>
+                  <m-button onClickevent={this.createNewItem}>增加项</m-button>
+                  <m-button>保存项</m-button>
+                  <m-button>删除整项</m-button>
+                </th>
               </table>
             );
           })}
