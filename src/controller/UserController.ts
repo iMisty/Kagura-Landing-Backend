00eee1aea8d5295337f2a47457e9c8195a7fe8d7
Miya <qianwumeng_2cyndi@qq.com>
2020-10-19 00:24:47 +0800

    Update UserController.ts

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -3,9 +3,28 @@
  * @Version: 1.0
  * @Date: 2020-10-18 16:53:30
  * @LastEditors: Miya
- * @LastEditTime: 2020-10-18 21:09:56
- * @Description: file content
+ * @LastEditTime: 2020-10-19 00:24:41
+ * @Description: 用户信息接口
  * @FilePath: /Kagura-Landing-Backend/src/controller/UserController.ts
  */
 const UserModel = require('../model/UserModel');
 const ContentModel = require('../model/ContactModel');
+
+class User{
+  // 创建管理员
+  public static async addNewAdmin(ctx: any){}
+
+  // 修改管理员密码
+  public static async updateAdmin(ctx: any){}
+
+  // 验证登录
+  public static async validateAdmin(ctx: any){}
+}
+
+// About 页面用接口
+class about extends User{
+  // 页面介绍文字
+  public static async setAboutIntro(ctx: any){
+    
+  }
+}
