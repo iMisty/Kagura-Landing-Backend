83d0b45b23fe22340b51d398295fb33bb6327b9c
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2020-11-03 16:22:57 +0800

    Fix: 修改获取数据方法为POST

--- src/router/info.ts
+++ src/router/info.ts
@@ -1,10 +1,10 @@
 /*
  * @Author: Miya
  * @Date: 2020-10-20 16:53:12
- * @LastEditTime: 2020-10-21 00:46:04
+ * @LastEditTime: 2020-11-03 15:55:34
  * @LastEditors: Miya
  * @Description: 不经过jwt验证部分
- * @FilePath: /Kagura-Landing-Backend/src/router/info.ts
+ * @FilePath: \LandingPagec:\Users\Platinum Prism\Documents\GitHub\Kagura-Landing-Backend\src\router\info.ts
  * @Version: 1.0
  */
 const router = require('koa-router')();
@@ -16,24 +16,24 @@ const Footer = require('../controller/FooterController');
 router.prefix('/info');
 
 // 获取文章列表
-router.get('/blog', Blog.getArticle);
+router.post('/blog', Blog.getArticle);
 
 // 根据ID获取单篇文章
-router.get('/blog/id', Blog.getArticleByID);
+router.post('/blog/id', Blog.getArticleByID);
 
 // 查找项目
-router.get('/work', Work.getWork);
+router.post('/work', Work.getWork);
 
 // 根据ID查找项目
-router.get('/work/id', Work.getWorkByID);
+router.post('/work/id', Work.getWorkByID);
 
 // 获取关于页介绍
-router.get('/about', User.getAboutIntro);
+router.post('/about', User.getAboutIntro);
 
 // 查阅所有团队信息
-router.get('/team', User.getTeamContact);
+router.post('/team', User.getTeamContact);
 
 // 获取页脚信息
-router.get('/footer', Footer.getFooterContent);
+router.post('/footer', Footer.getFooterContent);
 
 export default router;
