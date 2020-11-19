32675e29bba2802eaf7f4c53651c15603aeda9f1
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2020-11-19 18:10:54 +0800

    Update info.ts

--- src/router/info.ts
+++ src/router/info.ts
@@ -1,10 +1,10 @@
 /*
  * @Author: Miya
  * @Date: 2020-10-20 16:53:12
- * @LastEditTime: 2020-11-13 15:26:18
+ * @LastEditTime: 2020-11-19 17:51:25
  * @LastEditors: Miya
  * @Description: 不经过jwt验证部分
- * @FilePath: \Kagura-Landing-Backend\src\router\info.ts
+ * @FilePath: \Single-Search-APIc:\Users\Platinum Prism\Documents\GitHub\Kagura-Landing-Backend\src\router\info.ts
  * @Version: 1.0
  */
 const router = require('koa-router')();
@@ -16,27 +16,27 @@ const Footer = require('../controller/FooterController');
 router.prefix('/info');
 
 // 获取文章列表
-router.post('/blog', Blog.getArticle);
+router.get('/blog', Blog.getArticle);
 
 // 根据ID获取单篇文章
-router.post('/blog/id', Blog.getArticleByID);
+router.get('/blog/id', Blog.getArticleByID);
 
 // 查找项目
-router.post('/work', Work.getWork);
+router.get('/work', Work.getWork);
 
 // 根据ID查找项目
-router.post('/work/id', Work.getWorkByID);
+router.get('/work/id', Work.getWorkByID);
 
 // 获取关于页介绍
-router.post('/about', User.getAboutIntro);
+router.get('/about', User.getAboutIntro);
 
 // 查阅所有团队信息
-router.post('/team', User.getTeamContact);
+router.get('/team', User.getTeamContact);
 
 // 获取页脚信息
-router.post('/footer', Footer.getFooterContent);
+router.get('/footer', Footer.getFooterContent);
 
 // 验证token
-router.post('/admin', User.validateToken);
+router.get('/admin', User.validateToken);
 
 export default router;
